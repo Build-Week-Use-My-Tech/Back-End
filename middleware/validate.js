c00ca0a819f84996195cc2fa9a357a3acfb40f88
Elvis Knapman <elvis@elvisknapman.com>
2019-09-23 16:51:03 -0400

    :tada: wrote middleware to validate req body for...
    registering a new user

---
+++ middleware/validate.js
@@ -0,0 +1,17 @@
+// validate registration body
+
+const validateRegister = (req, res, next) => {
+  const { first_name, last_name, email, password } = req.body;
+
+  if (first_name && last_name && email && password) {
+    next();
+  } else {
+    res
+      .status(400)
+      .json({ message: "Must provide valid data for all required fields" });
+  }
+};
+
+module.exports = {
+  validateRegister
+};
