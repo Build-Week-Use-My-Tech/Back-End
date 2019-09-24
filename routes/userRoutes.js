d2f4df1cd5503502bca4d36e565e66f9d0e4ab46
Elvis Knapman <elvis@elvisknapman.com>
2019-09-24 13:06:45 -0400

    :sparkles: wrote handler for get user by ID

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,33 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "User Routes" });
+// // middleware
+const validate = require("../middleware/validate.js");
+const authenticate = require("../data/helpers/auth/auth-middleware.js");
+
+// helpers
+const Users = require("../data/helpers/users/users-model.js");
+
+router.get("/:id", authenticate, async (req, res) => {
+  const { id } = req.params;
+  const { email } = req;
+  console.log("req", req);
+  console.log("USER EMAIL", email);
+  try {
+    const user = await Users.returnUser(id);
+    console.log("USER EMAIL 2", user.email);
+
+    // only allow access if email on token matches the email of the requested user
+    if (user && email === user.email) {
+      res.status(200).json(user);
+    } else {
+      res.status(401).json({ message: "Invaid credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server encountered error trying to fetch user information"
+    });
+  }
 });
 
 module.exports = router;
