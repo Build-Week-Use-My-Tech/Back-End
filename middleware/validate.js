f88071c338884a3e0c232225d7c106b94fbb94cd
Elvis Knapman <elvis@elvisknapman.com>
2019-09-24 15:43:27 -0400

    :sparkles: updates

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,5 +1,5 @@
 // validate registration body
-const invalidBodyMsg = "Must provide valid data for all required fields"
+const invalidBodyMsg = "Must provide valid data for all required fields";
 
 const validateUserBody = (req, res, next) => {
   const { first_name, last_name, email, password, renter, owner } = req.body;
@@ -7,9 +7,7 @@ const validateUserBody = (req, res, next) => {
   if (first_name && last_name && email && password && renter && owner) {
     next();
   } else {
-    res
-      .status(400)
-      .json({ message: invalidBodyMsg });
+    res.status(400).json({ message: invalidBodyMsg });
   }
 };
 
@@ -19,33 +17,46 @@ const validateUpdateBody = (req, res, next) => {
   if (first_name && last_name && email && renter && owner) {
     next();
   } else {
-    res
-      .status(400)
-      .json({ message: invalidBodyMsg});
+    res.status(400).json({ message: invalidBodyMsg });
   }
 };
 
 // validate ad body
 
-function validateAd({body:{title, description, price, item_condition, item_available, negotiable}, method}, response, next){
-  if (method == "POST" || method == "PUT" || method == "DELETE"){
-    if(title &&
+function validateAd(
+  {
+    body: {
+      title,
+      description,
+      price,
+      item_condition,
+      item_available,
+      negotiable
+    },
+    method
+  },
+  response,
+  next
+) {
+  if (method == "POST" || method == "PUT" || method == "DELETE") {
+    if (
+      title &&
       description &&
       price &&
       item_condition &&
       item_available &&
-      negotiable){
-          next();
-      }else{
-        response.status(400).json({message:invalidBodyMsg});
-      }
-  }else{
+      negotiable
+    ) {
+      next();
+    } else {
+      response.status(400).json({ message: invalidBodyMsg });
+    }
+  } else {
     next();
   }
 }
 
 module.exports = {
-  validateRegister,
   validateAd,
   validateUserBody,
   validateUpdateBody
