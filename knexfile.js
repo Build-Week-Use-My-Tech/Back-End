cf8d294a59571645b612ac4a4a3f18c530b1b152
Elvis Knapman <elvis@elvisknapman.com>
2019-09-25 13:44:58 -0400

    :ambulance: fixed curly brace

--- knexfile.js
+++ knexfile.js
@@ -19,39 +19,39 @@ module.exports = {
         // runs after a connection is made to the sqlite engine
         conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       }
+    }
+  },
+
+  //   staging: {
+  //     client: "postgresql",
+  //     connection: {
+  //       database: "my_db",
+  //       user: "username",
+  //       password: "password"
+  //     },
+  //     pool: {
+  //       min: 2,
+  //       max: 10
+  //     },
+  //     migrations: {
+  //       tableName: "knex_migrations"
+  //     }
+  //   },
+
+  production: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
     },
 
-    //   staging: {
-    //     client: "postgresql",
-    //     connection: {
-    //       database: "my_db",
-    //       user: "username",
-    //       password: "password"
-    //     },
-    //     pool: {
-    //       min: 2,
-    //       max: 10
-    //     },
-    //     migrations: {
-    //       tableName: "knex_migrations"
-    //     }
-    //   },
-
-    production: {
-      client: "pg",
-      useNullAsDefault: true,
-      connection: process.env.DATABASE_URL,
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        directory: "./data/migrations"
-      },
-
-      seeds: {
-        directory: "./data/seeds"
-      }
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 };
